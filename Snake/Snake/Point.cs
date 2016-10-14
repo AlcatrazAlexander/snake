0cf04dff42ced7ac78a4e383338a466d1df9b3b4
AlcatrazAlexander <51tia2@mail.ru>
2016-10-14 20:08:13 +0300

    Encapsulation

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -12,6 +12,11 @@ namespace Snake
         public int y;
         public char sym;
 
+        public Point()
+        {
+
+        }
+
         public Point(int x, int y, char sym)
         {
             this.x = x;
